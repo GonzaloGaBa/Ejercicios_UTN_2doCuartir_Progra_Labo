a958d7eee4e5f75189d7ed2716eb216ef3d6afa7
GonzaloGaBa <gongabar@gmail.com>
2023-10-30 22:08:30 -0300

    Se agrego el ejercicio 01 y 02

---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Consigna
+            Ingresar 5 números por consola, guardándolos en una variable escalar.
+            Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+            */
+            const int numeroMaximosIngresado = 5;
+            int escalar;
+            int valorMaximo = int.MaxValue;
+            int valorMinimo = int.MinValue;
+            double suma = 0;
+
+            for (int i = 0; i < numeroMaximosIngresado; i++)
+            {
+                Console.WriteLine("Ingrese un numero");
+                string numeroIngresado = Console.ReadLine();
+
+                if (int.TryParse(numeroIngresado, out escalar))
+                {
+                    Console.WriteLine("Numero ingresado Correctamente");
+
+                    //Cacular maximo.
+                    if (escalar > valorMaximo)
+                    {
+                        valorMaximo = escalar;
+                    }
+
+                    //Calcular Minimo.
+                    if (escalar < valorMinimo)
+                    {
+                        valorMinimo = escalar;
+                    }
+
+                    suma += escalar;
+                }
+                else
+                {
+                    Console.WriteLine("Dato incorrecto");
+                }
+            }
+
+            double promedio = suma / numeroMaximosIngresado;
+
+            Console.WriteLine($"Valor Maximo: {valorMaximo}");
+            Console.WriteLine($"Valor Minimo: {valorMinimo}");
+            Console.WriteLine($"Valor Promedio: {promedio}");
+
+        }
+    }
+}
---
+++ Ejercicio02/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ejercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar un número y mostrar el cuadrado y el cubo del mismo.
+            //Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".
+
+            double numero;
+            int alCuadrado = 2;
+            int alCubo = 3;
+
+            Console.WriteLine("Ingrese un numero: ");
+            string numeroIngresado = Console.ReadLine();
+
+            if (double.TryParse(numeroIngresado, out numero))
+            {
+                Console.WriteLine("Dato numerico ingresado");
+                if (numero > 0)
+                {
+                    double resultadoElevadoAlCuadrado = Math.Pow(numero, alCuadrado);
+                    double resultadoElevadoAlCubo = Math.Pow(numero, alCubo);
+
+                    Console.WriteLine("Usted ingreso el numero {0}. \n " +
+                              "El resultado de elevarlo al cuadrado es de: {1}. \n " +
+                              "El resultado de elevarlo al cubo es de: {2}."
+                              , numero, resultadoElevadoAlCuadrado , resultadoElevadoAlCubo);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar número! \n Pd: Debe ser mayor que 0");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Dato ingresado Incorrectamente, debe Ingresar un numero");
+            }
+
+          
+        }
+    }
+}
